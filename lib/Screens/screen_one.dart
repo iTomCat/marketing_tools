f7bfefb826d8c52d456c3422edc144228e603171
Tomasz Kot M1 <tomaszkot.mail@gmail.com>
2022-11-14 20:20:59 +0100

    Logger added

--- lib/Screens/screen_one.dart
+++ lib/Screens/screen_one.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:logger/logger.dart';
 
 final _firestore = FirebaseFirestore.instance;
 
@@ -29,7 +30,14 @@ class _ScreenOneState extends State<ScreenOne> {
     docRef.get().then(
           (DocumentSnapshot doc) {
         final data = doc.data() as Map<String, dynamic>;
+
         debugPrint('Data From Firebase: $data');
+
+        //Logger.level = Level.warning;
+        //var logger = Logger( printer: PrefixPrinter(PrettyPrinter(colors: false)));
+        var logger = Logger();
+        logger.d("Logger is working!");
+        logger.wtf("Data From Firebase: $data");
         // ...
       },
       onError: (e) => print("Error getting document: $e"),
